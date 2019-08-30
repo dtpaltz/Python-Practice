bdcf4df868db4702620d28902db4ea8c723e1436
Dustin Paltz <dtpaltz@gmail.com>
2019-08-30 13:20:30 -0700

    1 additional interview problem edited. 1 additional daily problem worked.

--- Interview-Questions/IsNumberPalindrome.py
+++ Interview-Questions/IsNumberPalindrome.py
@@ -1,12 +1,17 @@
-
+# This problem has been asked by Blue Origin.
+#
+# Given a numbers, determine if the number is a palindrome without using any String data types.
+#
+# For example, 12321 is a palindrome, but 12325 is not
 
 def isNumberPalindrome(num: int):
     temp = num
     rev = 0
+    print(num)
 
     while temp != 0:
-        print('calc')
         rev = (rev * 10) + (temp % 10)
+        print(rev)
         temp = temp // 10
 
     if num == rev:
@@ -14,7 +19,8 @@ def isNumberPalindrome(num: int):
     else:
         print("number is not palindrome")
 
+    return num == rev
 
 
-isNumberPalindrome(12321)
+isNumberPalindrome(12345)
 
