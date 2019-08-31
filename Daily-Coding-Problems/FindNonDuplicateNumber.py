25ab14c765e50d85116aa1bfb53a437dfa4dd6bc
Dustin Paltz <dtpaltz@gmail.com>
2019-08-31 09:34:58 -0700

    Syncing files

---
+++ Daily-Coding-Problems/FindNonDuplicateNumber.py
@@ -0,0 +1,18 @@
+# This problem was recently asked by Facebook:
+#
+# Given a list of numbers, where every number shows up twice except for one number, find that one number.
+#
+# Challenge: Find a way to do this using O(1) memory.
+#
+# Example:
+# Input: [4, 3, 2, 4, 1, 3, 2]
+# Output: 1
+
+
+def singleNumber(nums):
+    pass
+
+
+print(singleNumber([4, 3, 2, 4, 1, 3, 2]))
+
+
